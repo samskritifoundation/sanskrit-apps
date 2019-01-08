741578c8c672e95659490de5e65b55a6ce85705d
pankaja-shree <pankajashree@gmail.com>
2019-01-08 17:29:05 +0530

    define api

--- server/models/Srutabodha.js
+++ server/models/Srutabodha.js
@@ -1,16 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const SrutabodhaSchema = new mongoose.Schema({
-  id: { type: String, unique: true },
-  title_sans: String,
-  title_eng: String,
-  definition_sans: String,
-  definition_eng: String,
-  common: String,
-  example_sans: String,
-  treeData: mongoose.Mixed
-}, { collection: 'srutabodha' });
+const SrutabodhaSchema = new mongoose.Schema(
+  {
+    id: { type: String, unique: true },
+    title_sans: String,
+    title_eng: String,
+    definition_sans: String,
+    definition_eng: String,
+    common: String,
+    example_sans: String,
+    treeData: mongoose.Mixed
+  },
+  { collection: 'srutabodha' }
+)
 
-const Srutabodha = mongoose.model('Srutabodha', SrutabodhaSchema);
+const Srutabodha = mongoose.model('Srutabodha', SrutabodhaSchema)
 
-module.exports = Srutabodha;
+module.exports = Srutabodha
