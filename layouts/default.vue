99da2d4431408232400d20f147e272ccaa737eb3
pankaja-shree <pankajashree@gmail.com>
2019-01-10 17:13:20 +0530

    change app layout

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,10 @@
 <template>
-  <v-app dark>
+  <v-app class = "papyrus">
     <v-navigation-drawer
-      :mini-variant="miniVariant"
+      :mini-variant.sync="miniVariant"
       :clipped="clipped"
       v-model="drawer"
+      class="info"
       fixed
       app
     >
@@ -16,47 +17,34 @@
           exact
         >
           <v-list-tile-action>
-            <v-icon v-html="item.icon" />
+            <v-icon v-html="item.icon"/>
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
+            <v-list-tile-title v-text="item.title"/>
           </v-list-tile-content>
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
     <v-toolbar
       :clipped-left="clipped"
+      dark
       fixed
       app
-    >
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>remove</v-icon>
-      </v-btn>
+      color="primary">
+      <v-toolbar-side-icon
+        color="primary"
+        @click="drawer = !drawer"/>
       <v-toolbar-title v-text="title"/>
+      <v-spacer/>
       <v-btn
+        flat
         icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>menu</v-icon>
+        to="/">
+        <v-icon>home</v-icon>
       </v-btn>
+      <span>Home</span>
     </v-toolbar>
-    <v-content>
+    <v-content class = "oldmap">
       <v-container>
         <nuxt />
       </v-container>
@@ -78,9 +66,10 @@
     </v-navigation-drawer>
     <v-footer
       :fixed="fixed"
-      app
-    >
-      <span>&copy; 2017</span>
+      color="primary"
+      dark
+      app>
+      <span>&copy; 2018</span>
     </v-footer>
   </v-app>
 </template>
@@ -90,17 +79,29 @@ export default {
   data() {
     return {
       clipped: false,
-      drawer: true,
+      drawer: false,
       fixed: false,
       items: [
-        { icon: 'apps', title: 'Welcome', to: '/' },
-        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+        { icon: 'info', title: 'About us', to: '/aboutsf' },
+        { icon: 'people', title: 'Credits', to: '/credits' },
+        { icon: 'contact_support', title: 'Contact us', to: '/contact' }
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Kriyānighaṇṭu'
     }
   }
 }
 </script>
+
+<style scoped>
+.oldmap {
+  background: url('~/assets/img/oldmap.jpg') no-repeat;
+  background-size: cover;
+}
+
+.text-light {
+  color: white;
+}
+</style>
