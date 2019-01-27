5b5ac9b891570e70059251b5f5f82b8f27300704
pankaja-shree <pankajashree@gmail.com>
2019-01-27 12:34:11 +0530

    insert header

---
+++ components/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-content>
+    <img
+      src="~assets/img/header1.png"
+      alt="logo" >
+  </v-content>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'example'
+    }
+  }
+}
+</script>
+
+<style scoped>
+img {
+  width: 100%;
+}
+</style>
