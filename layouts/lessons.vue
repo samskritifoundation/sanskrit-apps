5fd813f2d77199dad641f8df6f6b29a7671fa3e0
pankaja-shree <pankajashree@gmail.com>
2019-01-29 14:51:48 +0530

    add lessons page

---
+++ layouts/lessons.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-app class = "bg-image">
+    <Header />
+    <navleft :text_title="text_name" />
+    <nuxt />
+  </v-app>
+</template>
+
+<script>
+import navleft from '~/components/lessonsnav.vue'
+import Header from '~/components/header.vue'
+
+export default {
+  components: {
+    navleft,
+    Header
+  },
+  computed: {
+    text_name() {
+      // find name of text from path
+      return this.$route.path.split('/')[1]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.bg-image {
+  background: url('~assets/img/bg.png') no-repeat;
+  background-size: cover;
+  background-position: center;
+}
+
+.text-light {
+  color: white;
+}
+</style>
