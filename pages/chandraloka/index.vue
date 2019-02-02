50d43564fad5312e9e0493438741568f9433039b
pankaja-shree <pankajashree@gmail.com>
2019-02-02 10:58:53 +0530

    remove unwanted classes

--- pages/chandraloka/index.vue
+++ pages/chandraloka/index.vue
@@ -2,19 +2,20 @@
   <v-layout
     column
     justify-center
-    align-center
-    class="papyrus">
+    align-center>
     <img src="~assets/img/cl.png" >
     <v-flex
       xs12
       sm8
-      md8>
+      md8
+      align-center>
       <v-card
-        class = "bordered text-xs-center"
+        class = "text-xs-center"
         color = "secondary"
-        dark>
-        <v-card-title class="myheader2 devanagari text-xs-center"><h1>चन्द्रालोक-शिक्षकः - परस्परव्यवहारपूर्वकशिक्षणम्</h1></v-card-title>
-        <v-card-title class="myheader2 papyrus text-xs-center"><h2>Interactive tutorial for Candrāloka</h2></v-card-title>
+        dark
+        align-center>
+        <v-card-title><h1>चन्द्रालोक-शिक्षकः - परस्परव्यवहारपूर्वकशिक्षणम्</h1></v-card-title>
+        <v-card-title><h2>Interactive tutorial for Candrāloka</h2></v-card-title>
         <v-card-text>
           <h3>Interactive self learning software for learning rhetorics used in Sanskrit poetry.</h3>
           <hr class="my-3">
@@ -28,15 +29,15 @@
               :ripple="{ class: 'error--text' }"
               round
               color="accent darken-2"
-              class="text-capitalize font-weight-bold"
+              class="font-weight-bold"
               large> About <br> Candrāloka</v-btn>
             <v-card
               color="info"
-              class="papyrus bg1"
+              class="bg1"
               light>
-              <v-card-title class="myheader papyrus"><h1>Candrāloka</h1></v-card-title>
+              <v-card-title><h1>Candrāloka</h1></v-card-title>
               <v-divider/>
-              <v-card-text class="subheading papyrus font-weight-bold">
+              <v-card-text class="subheading font-weight-bold">
                 <p>The Candrāloka, authored by the great poet-devotee-scholar śri Jayadeva is one of the best works that throws light on the Figures of speech (popularly known as Alaṃkāra-s) that are a unique aspect of Sanskrit literature. 'śabda' and 'Artha' are the two aspects that are considered to be the body of literature (kāvya). Just as one adorns oneself with ear-rings, necklaces and such other ornaments to manifest the beauty of the human body, that shines forth on accounts of the ornaments, the body of the kāvya (literary work) too shines forth and becomes enjoyable when it is embellished with figures of speech that are aptly known as 'Alaṃkāra-s'.  That is why it has been mentioned 'काव्यशोभाकरान् धर्मान् अलङ्कारान् प्रचक्षते ' .</p>
 
                 <p>While there are several works that propound the definitions, give examples and explain the various Alaṃkāra-s uniquely employed in Sanskrit literature, the crest jewel among all such works is the Candrāloka of the exalted poet-scholar-devotee Jayadeva, who has written this book in an extremely simple, lucid and easily-understandable style. This book is studied as a primer by all beginners as well as scholars who are well-versed in the topic, for its depth and profoundness.  The same text is produced here with an English translation in the form of learnable modules.</p>
@@ -56,7 +57,7 @@
           <v-btn
             round
             color="accent darken-2"
-            class="font-weight-bold text-capitalize"
+            class="font-weight-bold"
             nuxt
             to="/chandraloka/lessons"
             large>अध्यायाः<br>Learn</v-btn>
@@ -64,7 +65,7 @@
           <v-btn
             round
             color="accent darken-2"
-            class="font-weight-bold text-capitalize"
+            class="font-weight-bold"
             nuxt
             to="/chandraloka/exercises"
             large>अभ्यासाः <br> Exercises</v-btn>
